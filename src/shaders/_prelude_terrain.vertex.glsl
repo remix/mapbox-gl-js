bd5a29411a4da6acec00604c490a690bd2eed9c9
Aleksandar Stojiljković <aleksandar.stojiljkovic@mapbox.com>
2021-04-08 17:39:07 +0300

    Terrain: shader support for sampling DEM elevation from float textures
    Feature is implemented in gl-native first. Since shader code is shared, code here is
    not used by gl-js (`DEM_FLOAT_FORMAT` is not defined).

--- src/shaders/_prelude_terrain.vertex.glsl
+++ src/shaders/_prelude_terrain.vertex.glsl
@@ -30,6 +30,10 @@ float decodeElevation(vec4 v) {
 }
 
 float currentElevation(vec2 apos) {
+#ifdef DEM_FLOAT_FORMAT
+    vec2 pos = (u_dem_size * (apos / 8192.0 * u_dem_scale + u_dem_tl) + 1.0) / (u_dem_size + 2.0);
+    return u_exaggeration * texture2D(u_dem, pos).a;
+#else
     float dd = 1.0 / (u_dem_size + 2.0);
     vec4 r = tileUvToDemSample(apos / 8192.0, u_dem_size, u_dem_scale, u_dem_tl);
     vec2 pos = r.xy;
@@ -41,9 +45,14 @@ float currentElevation(vec2 apos) {
     float br = decodeElevation(texture2D(u_dem, pos + vec2(dd, dd)));
 
     return u_exaggeration * mix(mix(tl, tr, f.x), mix(bl, br, f.x), f.y);
+#endif
 }
 
 float prevElevation(vec2 apos) {
+#ifdef DEM_FLOAT_FORMAT
+    vec2 pos = (u_dem_size * (apos / 8192.0 * u_dem_scale_prev + u_dem_tl_prev) + 1.0) / (u_dem_size + 2.0);
+    return u_exaggeration * texture2D(u_dem_prev, pos).a;
+#else
     float dd = 1.0 / (u_dem_size + 2.0);
     vec4 r = tileUvToDemSample(apos / 8192.0, u_dem_size, u_dem_scale_prev, u_dem_tl_prev);
     vec2 pos = r.xy;
@@ -55,6 +64,7 @@ float prevElevation(vec2 apos) {
     float br = decodeElevation(texture2D(u_dem_prev, pos + vec2(dd, dd)));
 
     return u_exaggeration * mix(mix(tl, tr, f.x), mix(bl, br, f.x), f.y);
+#endif
 }
 
 #ifdef TERRAIN_VERTEX_MORPHING
@@ -102,6 +112,12 @@ float occlusionFade(vec4 frag) {
  // This is so that rendering changes are reflected on CPU side for feature querying.
 
 vec4 fourSample(vec2 pos, vec2 off) {
+#ifdef DEM_FLOAT_FORMAT
+    float tl = texture2D(u_dem, pos).a;
+    float tr = texture2D(u_dem, pos + vec2(off.x, 0.0)).a;
+    float bl = texture2D(u_dem, pos + vec2(0.0, off.y)).a;
+    float br = texture2D(u_dem, pos + off).a;
+#else
     vec4 demtl = vec4(texture2D(u_dem, pos).xyz * 255.0, -1.0);
     float tl = dot(demtl, u_dem_unpack);
     vec4 demtr = vec4(texture2D(u_dem, pos + vec2(off.x, 0.0)).xyz * 255.0, -1.0);
@@ -110,6 +126,7 @@ vec4 fourSample(vec2 pos, vec2 off) {
     float bl = dot(dembl, u_dem_unpack);
     vec4 dembr = vec4(texture2D(u_dem, pos + off).xyz * 255.0, -1.0);
     float br = dot(dembr, u_dem_unpack);
+#endif
     return vec4(tl, tr, bl, br);
 }
 
